2db37d03d702a4e7725bdeeb9c2244b1899e79b3
DevSua <tndk5321@naver.com>
2021-06-09 13:11:17 +0900

    upload files

---
+++ memberBean.java
@@ -0,0 +1,41 @@
+package member;
+
+public class memberBean {
+	private String dbname;
+	private String dbemail;
+	private String dbhomepage;
+	private String dbsubject;
+	private String dbmemo;
+	public String getDbname() {
+		return dbname;
+	}
+	public void setDbname(String dbname) {
+		this.dbname = dbname;
+	}
+	public String getDbemail() {
+		return dbemail;
+	}
+	public void setDbemail(String dbemail) {
+		this.dbemail = dbemail;
+	}
+	public String getDbhomepage() {
+		return dbhomepage;
+	}
+	public void setDbhomepage(String dbhomepage) {
+		this.dbhomepage = dbhomepage;
+	}
+	public String getDbsubject() {
+		return dbsubject;
+	}
+	public void setDbsubject(String dbsubject) {
+		this.dbsubject = dbsubject;
+	}
+	public String getDbmemo() {
+		return dbmemo;
+	}
+	public void setDbmemo(String dbmemo) {
+		this.dbmemo = dbmemo;
+	}
+	
+
+	}
